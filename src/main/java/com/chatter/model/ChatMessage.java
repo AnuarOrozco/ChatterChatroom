de204106e4b876f57b710c8f512f692f1aa366cf
Anuar Orozco <anuarorozzco@gmail.com>
2024-12-28 13:34:08 -0600

    model-package: For some reason Lombok annotations was not getting recognized so I had to manually created the getters and setters

--- src/main/java/com/chatter/model/ChatMessage.java
+++ src/main/java/com/chatter/model/ChatMessage.java
@@ -1,15 +1,27 @@
 package com.chatter.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter @Setter
 @AllArgsConstructor @NoArgsConstructor
 public class ChatMessage {
 
     private String username;
     private String content;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
